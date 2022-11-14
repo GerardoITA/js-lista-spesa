0ede3de85ee3adb57187605157fd22f5580b6089
GerardoITA <gerardo.giusti@tiscali.it>
2022-11-14 17:05:04 +0100

    for completato

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ let lista = ["Pasta", "Formaggio", "Carne", "Verdura"];
 let div = document.getElementById("paint");
 
 
-for (i = 0; i<lista.length; i++){
+/*for (i = 0; i<lista.length; i++){
 
     div.innerHTML += `
         <li>${lista[i]}</li>
     `; 
-}
+}*/
+
+let i = 0;
+
+ while (i < lista.length){
+    
+    div.innerHTML += `
+        <li>${lista[i]}</li>
+    `; 
+
+    i++;
+} 
